323cb1ef9fd2d83e1422e9fcf9bd017f05e22b32
kp-aris-dascalu <aris.dascalu@kape.com>
2025-10-20 14:57:04 +0300

    Initial commit, initial structure of the project, along with some page structure: landing page, login, menu and so on

---
+++ Backend/location_check.py
@@ -0,0 +1,6 @@
+import customer_location from Data/data_location
+def location_check(ip_location):
+    if location in customer_location:
+        return True
+    else:
+        return False
---
+++ Data/users_data.sql
@@ -0,0 +1,7 @@
+user_id=
+    1: {"username": "admin_1", "email": "", "password": "admin1234", id: "7456" },
+    2: {"username": "admin_2", "email": "", "password": "admin2134", id: "8564" },
+    3: {"username": "admin_3", "email": "", "password": "admin3124", id: "9675" },
+    4: {"username": "admin_4", "email": "", "password": "admin4123", id: "4637" },
+    5: {"username": "", "email": "", "password": "" }
+}      
