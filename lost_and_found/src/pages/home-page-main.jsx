c8fd6b75a1f78aecda913db38bf50b2a7058c327
kp-aris-dascalu <aris.dascalu@kape.com>
2025-10-29 17:03:24 +0200

    [WEB-2] Added the react library, along with mongodb server and ip checker script using node. Also npm packets

---
+++ lost_and_found/src/pages/home-page-main.jsx
@@ -0,0 +1,117 @@
+import { useState, useRef, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../styles/CSS/home_page_main.css';
+
+function HomePageMain() {
+  // State to control menu open/closed
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [height, setHeight] = useState('0px');
+  const panelRef = useRef(null);
+
+  // Mouse enter handler - opens menu
+  const openNav = () => {
+    setIsMenuOpen(true);
+    if (panelRef.current) {
+      setHeight(panelRef.current.scrollHeight + 'px');
+    }
+  };
+
+  // Mouse leave handler - closes menu
+  const closeNav = () => {
+    setIsMenuOpen(false);
+    if (panelRef.current) {
+      setHeight('0px');
+    }
+  };
+
+  // Handle transition end effect
+  useEffect(() => {
+    const panel = panelRef.current;
+    
+    const handleTransitionEnd = () => {
+      if (!isMenuOpen) {
+        setHeight('0px');
+      }
+    };
+
+    if (panel) {
+      panel.addEventListener('transitionend', handleTransitionEnd);
+      return () => panel.removeEventListener('transitionend', handleTransitionEnd);
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <div className="container">
+      <div className="navigation-bar">
+        <div className="topnav-left">
+          <div className="logo">
+            <div className="homebbt">
+              <Link to="/home">
+                <button className="home-button">Home</button>
+              </Link>
+            </div>
+            
+            {/* News button - standalone */}
+            <div className="newsbt">
+              <Link to="/news-page">
+                <button className="news-button">News</button>
+              </Link>
+            </div>
+            
+            {/* Menu button - separate from News */}
+            <div className="menu">
+              <button 
+                className="button-menu" 
+                onMouseEnter={openNav}
+                onMouseLeave={closeNav}
+              >
+                Menu
+              </button>
+              <div 
+                ref={panelRef}
+                className={`menu-open ${isMenuOpen ? 'is-open' : ''}`}
+                id="menu-trans"
+                style={{ height: height }}
+                onMouseEnter={openNav}
+                onMouseLeave={closeNav}
+              >
+                <Link to="/settings">Settings</Link>
+                <Link to="/login">Account</Link>
+                <Link to="/donations">Donations</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div className="topnav-right">
+          <div className="loginbt">
+            <Link to="/login">
+              <button className="login-button">Sign Up</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      
+      <div className="login-welcome-message">
+        <div className="motto-message">
+          <h2>You lost it, We found it!</h2>
+        </div>
+        <div className="connect-message">
+          <h3>Connect with us!</h3>
+        </div>
+      </div>
+      
+      <div className="login-table"></div>
+      
+      <div className="image">
+        <img 
+          src="/Pictures/login_phone.png" 
+          alt="Login Phone" 
+          className="img_login_phone" 
+        />
+      </div>
+    </div>
+  );
+}
+
+export default HomePageMain;
