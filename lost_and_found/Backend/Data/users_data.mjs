d22753b70762c497192476582b2d1e5887b324a0
Cristi N <cristi@Laptop-Cristi.local>
2025-11-04 12:09:02 +0200

    v1.0.1 - fixed login/register backend and cleaned App.js imports

--- lost_and_found/Backend/Data/users_data.mjs
+++ lost_and_found/Backend/Data/users_data.mjs
@@ -1,5 +1,5 @@
 user_id={
-    1: {"username": "cristi", "email": "asd", "password": "123", id: "" },
+    1: {"username": "", "email": "", "password": "", id: "" },
     2: {"username": "", "email": "", "password": "", id: "" },
     3: {"username": "", "email": "", "password": "", id: "" },
     4: {"username": "", "email": "", "password": "", id: "" },
